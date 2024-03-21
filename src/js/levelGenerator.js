2d7f2abf9d422e3c04b3e8aa110689243ce136e6
DmitrySkripunov <skripunov@mindbox.cloud>
2024-03-21 18:01:08 +0400

    center brackets

--- src/js/levelGenerator.js
+++ src/js/levelGenerator.js
@@ -1,30 +1,38 @@
 import { game } from "melonjs";
 import BlockEntity from "./renderables/block";
-import { BLOCK_SIZE } from "../constants/constants";
+import { BLOCK_SIZE, BRICK_KINDS } from "../constants/constants";
 
 const GAP = 2;
 const TOP = BLOCK_SIZE[1];
 const LEFT = BLOCK_SIZE[0];
-const BLOCKS_COUNT = 27;
 
 export const generateLevel = () => {
+  game.viewport.width;
+
   let top = 0;
-  let left = 0;
+
   const ROWS = 4;
   const COLS = 10;
 
+  const initialLeft =
+    (game.viewport.width - (LEFT * (COLS + 2) + GAP * (COLS + 1))) / 2;
+
+  let left = initialLeft;
+
   const blocks = [];
   let id = 1;
-  for (let row = 0; row < ROWS; row++) {
+  for (let row = 1; row <= ROWS; row++) {
     top += TOP;
-    for (let col = 0; col < COLS; col++) {
+    for (let col = 1; col <= COLS; col++) {
       left += LEFT;
-      blocks.push(new BlockEntity(left, top, { id: ++id, color: "green" }));
-      if (col < COLS - 1) left += GAP;
+      blocks.push(
+        new BlockEntity(left, top, { id: ++id, color: BRICK_KINDS[row].color })
+      );
+      if (col < COLS) left += GAP;
     }
 
-    left = 0;
-    if (row < ROWS - 1) top += GAP;
+    left = initialLeft;
+    if (row < ROWS) top += GAP;
   }
 
   blocks.forEach((block) => {
